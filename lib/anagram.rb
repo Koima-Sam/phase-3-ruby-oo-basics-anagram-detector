24fa1207730f20a75a92f928cbb1b04caa4813c0
Koima-Sam <sam17koima@gmail.com>
2022-08-23 01:21:05 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize word
+        @word = word
+    end
+    def match array_names
+        match_words = []
+            array_names.each do |item|
+                if @word.chars.sort == item.chars.sort
+                    match_words << item
+                end
+            end
+        match_words
+
+    end
+end
